732a6632bdefde63d07c363ec3efaed3e080fa69
Abhishek Kasireddy <Akasireddy99@gmail.com>
2020-02-17 09:32:51 +0530

    Updates

---
+++ precache-manifest.9d7fa298627a92298ed12fb20d64ef70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1ea00294a0ce0f78ff3f90928dffd2",
+    "url": "/Minesweeper/index.html"
+  },
+  {
+    "revision": "542f1007b84441a35343",
+    "url": "/Minesweeper/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "02c38b815fab3f62e256",
+    "url": "/Minesweeper/static/css/main.7d03eea4.chunk.css"
+  },
+  {
+    "revision": "542f1007b84441a35343",
+    "url": "/Minesweeper/static/js/2.38802a73.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/Minesweeper/static/js/2.38802a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02c38b815fab3f62e256",
+    "url": "/Minesweeper/static/js/main.b004ba94.chunk.js"
+  },
+  {
+    "revision": "94dbed607e114afd08ef",
+    "url": "/Minesweeper/static/js/runtime-main.de26a4e6.js"
+  }
+]);
